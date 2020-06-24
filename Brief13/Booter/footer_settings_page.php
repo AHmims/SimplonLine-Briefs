a8de838e12f43482c3d5210b628dfd2274ac08ca
AHmims <ali.hmims99@gmail.com>
2020-06-24 02:26:27 +0100

    init

---
+++ Brief13/Booter/footer_settings_page.php
@@ -0,0 +1,60 @@
+<?php
+global $chk;
+if (isset($_POST['wphw_submit'])) {
+    wphw_opt();
+}
+function wphw_opt()
+{
+    $foterText = $_POST['footertextname'];
+    $fbText = $_POST['facebookURL'];
+    $twtText = $_POST['twitterURL'];
+    // 
+    // 
+    global $chk, $fbURI, $twtURI;
+    if (get_option('footer_text') != trim($foterText))
+        $chk = update_option('footer_text', trim($foterText));
+    if (get_option('facebook_URL') != trim($fbText))
+        $fbText = update_option('facebook_URL', trim($fbText));
+    if (get_option('twitter_URL') != trim($twtText))
+        $twtText = update_option('twitter_URL', trim($twtText));
+}
+?>
+<div class="wrap">
+    <div id="icon-options-general" class="icon32"> <br>
+    </div>
+    <h2>Footer Settings</h2>
+    <?php if (isset($_POST['wphw_submit'])) : ?>
+        <div id="message" class="updated below-h2">
+            <p>Content updated successfully</p>
+        </div>
+    <?php endif; ?>
+    <div class="metabox-holder">
+        <div class="postbox">
+            <h3><strong>Enter footer text and click on save button.</strong></h3>
+            <h3>
+                <form method="post" action="">
+                    <table class="form-table">
+                        <tr>
+                            <th scope="row">Footer Text</th>
+                            <td><input type="text" name="footertextname" value="<?php echo get_option('footer_text'); ?>" style="width:350px;" /></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Facebook URL</th>
+                            <td><input type="text" name="facebookURL" value="<?php echo get_option('facebook_URL'); ?>" style="width:350px;" /></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Twitter URL</th>
+                            <td><input type="text" name="twitterURL" value="<?php echo get_option('twitter_URL'); ?>" style="width:350px;" /></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">&nbsp;</th>
+                            <td style="padding-top:10px;  padding-bottom:10px;">
+                                <input type="submit" name="wphw_submit" value="Save changes" class="button-primary" />
+                            </td>
+                        </tr>
+                    </table>
+                </form>
+            </h3>
+        </div>
+    </div>
+</div>
---
+++ Brief13/README.md
@@ -0,0 +1,25 @@
+# Brief 13
+
+Ce plugin ajoute un footer en bas des pages.
+
+## Installation
+
+1. Copiez le dossier Booter puis le placez dans le chemain suivant : "VotreSiteWeb"/wp-content/plugins
+2. Ouvrez la page admin dans votre navigateur, puis clickez sur "Plugins" -> "Installed plugins"
+3. Activez le plugins sous le nom Booter
+4. Une nouvelle option sous le nom Booter a été ajoutée, clickez le
+5. Remplir les champs puis clickez sur le button "Save changes".
+
+## Captures d'écran
+
+<p align="center">
+  <img src="./screen.png" alt="Plugin in menu">
+</p>
+
+<p align="center">
+  <img src="./screen1.png" alt="Plugin interface">
+</p>
+
+<p align="center">
+  <img src="./screen2.png" alt="In action">
+</p>
